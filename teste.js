7cefee95b0e242e857692e815b3237850f0edf2f
JulioHelo <112554757+JulioHelo@users.noreply.github.com>
2022-09-14 09:32:49 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -2,7 +2,21 @@ const express = require("express");
 
 const app = express();
 
+app.get("/",function (req , resp){
+
+    resp.send("Seja Bem-vindo ao meu app")
+});
+
+app.get("/contato",function (req , resp){
+
+    resp.send("Pagina de contado do meu app")
+});
+
+app.get("/Produto",function (req , resp){
+
+    resp.send("Pagina do produto  contado do meu app")
+});
+
 app.listen(8081, function(){
     console.log("O servidor está funcionando na url http://localhost:8081");
 });
-
